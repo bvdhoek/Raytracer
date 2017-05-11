62d9cad25bab999a03d7551d590a6fbcfabfcf5e
Maaike <maaikegalama@outlook.com>
2017-05-11 15:43:13 +0200

    camera class

---
+++ RayTracer/Camera.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayTracer
+{
+    class Camera
+    {
+        public Vector3 position;
+
+    }
+}
