254e19273e7d56d33204b86cf96ea93508cbdf60
Maaike <maaikegalama@outlook.com>
2017-05-11 16:43:53 +0200

    Added raytracer and implemented makeray method in camera

--- RayTracer/Camera.cs
+++ RayTracer/Camera.cs
@@ -1,14 +1,32 @@
-﻿using System.Numerics;
+﻿using System;
+using System.Numerics;
 
 namespace RayTracer
 {
     class Camera
     {
         // Camera position
-        public Vector3 pos = new Vector3(0, 0, 0);
+        Vector3 pos = new Vector3(0, 0, 0);
 
         // Camera direction
-        public Vector3 d = new Vector3(0, 0, 1);
+        Vector3 d = new Vector3(0, 0, 1);
 
-    }
+        Vector3 screenCenter;
+
+        // Screen corners:
+        Vector3 p0, p1, p2;
+        public Camera()
+        {
+            this.screenCenter = pos + 10 * d;
+            p0 = pos + new Vector3(-1, -1,0);
+            p1 = pos + new Vector3(1, -1, 0);
+            p2 = pos + new Vector3(-1, 1, 0);
+        }
+
+        internal Ray MakeRay(float x, float y)
+        {
+            Vector3 screenLocation = p0 + x * (p1 - p0) + y * (p2 - p0);
+            Vector3 direction = Vector3.Normalize(screenLocation - this.pos);            return new Ray(pos, direction, 1);
+        }
+}
 }
---
+++ RayTracer/RayTracer.cs
@@ -0,0 +1,28 @@
+﻿
+//Raytracer, which owns the scene, camera and the display surface.The Raytracer implements a
+//method Render, which uses the camera to loop over the pixels of the screen plane and to
+//generate a ray for each pixel, which is then used to find the nearest intersection.The result is
+//then visualized by plotting a pixel. For one line of pixels (typically line 256 for a 512x512
+//window), it generates debug output by visualizing every Nth ray(where N is e.g. 10).
+namespace RayTracer
+{
+    class RayTracer
+    {
+        Camera camera;
+        Scene scene;
+
+        void Render()
+        {
+            for (int i = 0; i < 512; i++)
+            {
+                for (int j = 0; j < 512; j++)
+                {
+                    float x = ((float)i) / 512;
+                    float y = ((float)i) / 512;
+                    Ray ray = camera.MakeRay(x, y);
+                    scene.Intersect(ray);
+                }
+            }
+        }
+    }
+}
