daeba0e72f0c1f61fa9ec1cda09cbf79f79de9b4
Bernd van den Hoek <bernd_hoek@hotmail.com>
2017-05-11 15:59:36 +0200

    Add ray struct

---
+++ RayTracer/ray.cs
@@ -0,0 +1,18 @@
+﻿using System.Numerics;
+
+namespace RayTracer
+{
+    struct Ray
+    {
+        Vector3 d; // ray direction
+        Vector3 o; // ray origin
+        float t; // ray length
+
+        public Ray(Vector3 origin, Vector3 direction, float length)
+        {
+            d = Vector3.Normalize(direction);
+            o = origin;
+            t = length;
+        }
+    }
+}
