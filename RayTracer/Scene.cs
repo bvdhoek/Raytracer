7fb2beb9bc4cb610c31354ed016b24fe9982aa47
Maaike <maaikegalama@outlook.com>
2017-05-16 15:14:02 +0200

    Multiple lightsources supported

--- RayTracer/Scene.cs
+++ RayTracer/Scene.cs
@@ -3,8 +3,8 @@ using System.Numerics;
 
 namespace RayTracer
 {
-    //    Scene, which stores a list of primitives and light sources.It implements a scene-level Intersect
-    //method, which loops over the primitives and returns the closest intersection
+    // Scene, which stores a list of primitives and light sources.It implements a scene-level Intersect
+    // method, which loops over the primitives and returns the closest intersection
     class Scene
     {
         private Primitive[] primitives = new Primitive[1];
@@ -36,13 +36,26 @@ namespace RayTracer
             return nearest;
         }
 
+        // Get the illumination for all light sources
         internal Vector3 DirectIllumination(Intersection intersect)
+        {
+            Vector3 color = new Vector3();
+
+            for (int i = 0; i < lights.Length; i++)
+            {
+                color += CalculateIllumination(intersect, i);
+            }
+            return color;
+        }
+
+        // Get illumination for lightsource with index i
+        private Vector3 CalculateIllumination(Intersection intersect, int i)
         {
             Ray shadowRay = new Ray();
             shadowRay.origin = intersect.intersectionPoint;
 
             // Keep an non-normalized directon in case we need to calculate length later
-            Vector3 rayDirection = lights[0].pos - intersect.intersectionPoint;
+            Vector3 rayDirection = lights[i].pos - intersect.intersectionPoint;
 
             // Normalize for calulating intersections
             shadowRay.direction = Vector3.Normalize(rayDirection);
@@ -50,14 +63,13 @@ namespace RayTracer
             // Offset the origin by a small margin
             shadowRay.origin += 0.001f * shadowRay.direction;
 
-            for (int i = 0; i < primitives.Length; i++)
+            //  Check if any primitives intersect with this shadowray
+            if (Intersect(shadowRay) != null)
             {
-                if (Intersect(shadowRay) != null)
-                {
-                    // Intersection point is in the shadow, return black.
-                    return new Vector3(0, 0, 0);
-                }
+                // Intersection point is in the shadow, return black.
+                return new Vector3(0, 0, 0);
             }
+
             // No intersection happened so we can calculate light color/intensity:
             float dist = rayDirection.Length();
             float attenuation = 1 / (dist * dist);
