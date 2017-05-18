862a60e0ccf167d0ea4688bc21fb3d8bf031a508
Bernd van den Hoek <bernd_hoek@hotmail.com>
2017-05-18 16:00:39 +0200

    Add plane

--- RayTracer/Scene.cs
+++ RayTracer/Scene.cs
@@ -7,7 +7,7 @@ namespace RayTracer
     // method, which loops over the primitives and returns the closest intersection
     class Scene
     {
-        private Primitive[] primitives = new Primitive[3];
+        private Primitive[] primitives = new Primitive[4];
         private Light[] lights = new Light[1];
 
         // Create new scene and populate with some default lights and primitives.
@@ -23,6 +23,8 @@ namespace RayTracer
 
             spherePos.X += 4;
             this.primitives[2] = new Sphere(spherePos, 0.5f, new Vector3(0, 0, 1));
+
+            primitives[3] = new Plane(new Vector3(0, 1, 0), new Vector3(0, -1, 0), new Vector3(0, 1, 1));
         }
 
         // Find intersection of the ray with nearest object in the scene.
